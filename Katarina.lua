4fb5010d233fefbfffb04b7605f1e7473e271503
ShabbaWind <sanyiwind@gmail.com>
2016-07-06 19:49:57 +0200

    Add files via upload

--- Katarina.lua
+++ Katarina.lua
@@ -1,5 +1,17 @@
 if myHero.charName ~= "Katarina" then return end
-  
+
+local versionGOE = 1.02
+local SCRIPT_NAME_GOE = "TheWind Fasza Jump"
+local needUpdate_GOE = false
+local needRun_GOE = true
+AddTickCallback(UpdateScriptGOE)
+local GlobalJumpAbility = _W
+local JumpAbilityRange = 600
+if myHero.charName == "Katarina" then GlobalJumpAbility = _E JumpAbilityRange = 600 end																																																																																																																															 if myHero.charName == "Jax" then GlobalJumpAbility = _Q JumpAbilityRange = 600 end if myHero.charName == "LeeSin" then GlobalJumpAbility = _W JumpAbilityRange = 600 end
+local Wards = {}
+local LastJump = 0
+local Jumped = false
+local LastPlacedObject = 0
 local HK = 32
 local HHK = 84
 local range = 700
@@ -22,40 +34,89 @@ local RDY = {Q = false, W = false, E = false, R = false, I = false, DFG = false,
 
 function OnLoad()
         KCConfig = scriptConfig("The Wind Katarina", "katarinacombo")
-        KCConfig:addParam("scriptActive", "Kombo", SCRIPT_PARAM_ONKEYDOWN, false, HK)
-        KCConfig:addParam("harass", "Tamadas (Nem teljes Comb)", SCRIPT_PARAM_ONKEYDOWN, false, HHK)
-        KCConfig:addParam("harasscombo", "Tamadas (Nem teljes Comb) beallitas", SCRIPT_PARAM_SLICE, 2, 0, 2, 0)
-        KCConfig:addParam("drawcircles", "Korok mutatasa", SCRIPT_PARAM_ONOFF, true)
-        KCConfig:addParam("drawtext", "Szoveg mutatasa", SCRIPT_PARAM_ONOFF, true)
-        KCConfig:addParam("useult", "Ulti hasznalata", SCRIPT_PARAM_ONOFF, true)
-        KCConfig:addParam("delayw", "Passzivos W", SCRIPT_PARAM_ONOFF, true)
-        KCConfig:addParam("canstopult", "Megallithato ulti", SCRIPT_PARAM_ONOFF, true)
-        KCConfig:addParam("autoIgnite", "Automatikus Ignite", SCRIPT_PARAM_ONOFF, true)
-        KCConfig:permaShow("scriptActive")
-        KCConfig:permaShow("harass")
-        ts = TargetSelector(TARGET_LESS_CAST_PRIORITY,range,DAMAGE_MAGIC)
-        ts.name = "Katarina"
-        KCConfig:addTS(ts)
+		KCConfig:addSubMenu("Katarina beallitasok", "Kata")
+        KCConfig.Kata:addParam("scriptActive", "Kombo", SCRIPT_PARAM_ONKEYDOWN, false, HK)
+        KCConfig.Kata:addParam("harass", "Tamadas (Nem teljes Comb)", SCRIPT_PARAM_ONKEYDOWN, false, HHK)
+        KCConfig.Kata:addParam("harasscombo", "(Nem teljes Comb) beallitas", SCRIPT_PARAM_SLICE, 2, 0, 2, 0)
+        KCConfig.Kata:addParam("drawcircles", "Korok mutatasa", SCRIPT_PARAM_ONOFF, true)
+        KCConfig.Kata:addParam("drawtext", "Szoveg mutatasa", SCRIPT_PARAM_ONOFF, true)
+        KCConfig.Kata:addParam("useult", "Ulti hasznalata", SCRIPT_PARAM_ONOFF, true)
+        KCConfig.Kata:addParam("delayw", "Passzivos W", SCRIPT_PARAM_ONOFF, true)
+        KCConfig.Kata:addParam("canstopult", "Megallithato ulti", SCRIPT_PARAM_ONOFF, true)
+        KCConfig.Kata:addParam("autoIgnite", "Automatikus Ignite", SCRIPT_PARAM_ONOFF, true)
+		
+
+		KCConfig:addSubMenu("Automata skill elosztas", "AutoLeveler")
+		KCConfig.AutoLeveler:addParam("Active", "Automatikus skillpont kiosztas", SCRIPT_PARAM_ONOFF, true)
+		
+		
+		KCConfig:addSubMenu("Ward Ugras", "WardUgras")
+		KCConfig.WardUgras:addParam("drawJumpRange","Ugras range", SCRIPT_PARAM_ONOFF, false)
+		KCConfig.WardUgras:addParam("maxdistance","Ugrasi nagysag", SCRIPT_PARAM_SLICE, 200, 0, 500, 0)
+		KCConfig.WardUgras:addParam("drawPlacedRange","Ugras kor range", SCRIPT_PARAM_ONOFF, true)
+		KCConfig.WardUgras:addParam("JumpKey","Ugras gomb", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("G"))
+		KCConfig.WardUgras.JumpKey = false
         if myHero:GetSpellData(SUMMONER_1).name:find("SummonerDot") then Slot.I = SUMMONER_1
         elseif myHero:GetSpellData(SUMMONER_2).name:find("SummonerDot") then Slot.I = SUMMONER_2 end
         for i=1, heroManager.iCount do waittxt[i] = i*3 end
+        ts = TargetSelector(TARGET_LESS_CAST_PRIORITY,range,DAMAGE_MAGIC)
+        ts.name = "TheWind"
+        KCConfig:addTS(ts)
 		Katarina()
+
+		KCConfig:addSubMenu("Skin valto", "Skin")
+		KCConfig.Skin:addParam("skin", "Bekapcsolva", SCRIPT_PARAM_ONOFF, true)
+		KCConfig.Skin:addParam("skinlist", "Valassz Skint", SCRIPT_PARAM_LIST, 1, {"1","2","3","4","5","6","7","8","9","10","11","12","13"})
+		KCConfig.Skin:setCallback("skin", function (value)
+		if value then
+			SetSkin(myHero, KCConfig.Skin.skinlist-1)
+		else
+			SetSkin(myHero, -1)
+			end
+		end)
+		KCConfig.Skin:setCallback("skinlist", function (value)
+			if KCConfig.Skin.skin then
+				SetSkin(myHero, KCConfig.Skin.skinlist-1)
+				end
+			end)
 end
 
 class 'Katarina'
 
 function Katarina:__init()
-	self.Version = 1.1
+	self:LoadPriorityTable()
+	self:SetTablePriorities()
+	self.Version = 1.2
 	self:SendMsg("[Betoltott verzio: "..self.Version.."]")
 	self:CheckUpdate()
+	self.LastSpell = 0
+	self.Sequence = {1,2,3,1,1,4,2,2,1,2,4,1,2,3,3,4,3,3}
+	self:Menu()
+	self:Callbacks()
 end
 
 function Katarina:SendMsg(msg)
 	PrintChat("<font color=\"#831928\"><b>[The Wind Katarina Script]</b></font> ".."<font color=\"#00D2FF\"><b>"..msg.."</b></font>")
 end
- 
 
+function Katarina:Callbacks()
+	AddTickCallback(function() self:AutoLeveler() end)
+end
+
+function Katarina:Menu()
+	
+end
+
+function Katarina:AutoLeveler()
+  	if KCConfig.AutoLeveler.Active and os.clock() - self.LastSpell >= 1.0 then
+  	self.LastSpell = os.clock()  
+  	DelayAction(function() autoLevelSetSequence(self.Sequence) end,0.5)
+        end
+	if not KCConfig.AutoLeveler.Active then autoLevelSetSequence(nil) end
+end
+	
 function OnTick()
+		WardJump()
         ts:update()
         for name,number in pairs(ID) do Slot[name] = GetInventorySlotItem(number) end
         for name,state in pairs(RDY) do RDY[name] = (Slot[name] ~= nil and myHero:CanUseSpell(Slot[name]) == READY) end
@@ -220,6 +281,11 @@ function OnDraw()
                         DrawCircle3D(ts.target.x, ts.target.y, ts.target.z, ts.target.boundingRadius, 8, ARGB(200,255,155,0), 20)
                 end
         end
+		if KCConfig.WardUgras.drawJumpRange then DrawCircle(myHero.x, myHero.y, myHero.z, 600, RGB(0,191,255)) end
+		if KCConfig.WardUgras.drawPlacedRange and KCConfig.WardUgras.JumpKey and KCConfig.WardUgras.maxdistance ~= 0 then
+			local jumpTo = GetDistance(mousePos) < JumpAbilityRange and Point(mousePos.x, mousePos.z) or Point(myHero.x, myHero.z)-(Point(myHero.x, myHero.z)-Point(mousePos.x, mousePos.z)):normalized()*JumpAbilityRange
+			DrawCircle(jumpTo.x, mousePos.y, jumpTo.y, KCConfig.WardUgras.maxdistance, RGB(0,191,255))
+		end
         for i=1, heroManager.iCount do
                 local enemydraw = heroManager:GetHero(i)
                 if ValidTarget(enemydraw) then
@@ -245,6 +311,64 @@ function OnDraw()
                 else waittxt[i] = waittxt[i]-1 end
         end
 end
+
+	function Katarina:LoadPriorityTable()
+		--|> This bish is long for sake of cleaness is here
+		self.priorityTable = {
+			AP = {
+				'Annie', 'Ahri', 'Akali', 'Anivia', 'Annie', 'Azir', 'Brand', 'Cassiopeia', 'Diana', 'Evelynn', 'FiddleSticks', 'Fizz', 'Gragas', 'Heimerdinger', 'Karthus',
+				'Kassadin', 'Katarina', 'Kayle', 'Kennen', 'Leblanc', 'Lissandra', 'Lux', 'Malzahar', 'Mordekaiser', 'Morgana', 'Nidalee', 'Orianna',
+				'Ryze', 'Sion', 'Swain', 'Syndra', 'Teemo', 'TwistedFate', 'Veigar', 'Viktor', 'Vladimir', 'Xerath', 'Ziggs', 'Zyra'
+			},
+			Support = {
+				'Alistar', 'Blitzcrank', 'Braum', 'Janna', 'Karma', 'Leona', 'Lulu', 'Nami', 'Nunu', 'Sona', 'Soraka', 'Taric', 'Thresh', 'Zilean'
+			},
+			Tank = {
+				'Amumu', 'Chogath', 'DrMundo', 'Galio', 'Hecarim', 'Malphite', 'Maokai', 'Nasus', 'Rammus', 'Sejuani', 'Nautilus', 'Shen', 'Singed', 'Skarner', 'Volibear',
+				'Warwick', 'Yorick', 'Zac'
+			},
+			AD_Carry = {
+				'Ashe', 'Caitlyn', 'Corki', 'Draven', 'Ezreal', 'Graves', 'Jayce', 'Jinx', 'Kalista', 'KogMaw', 'Lucian', 'MasterYi', 'MissFortune', 'Pantheon', 'Quinn', 'Shaco', 'Sivir',
+				'Talon','Tryndamere', 'Tristana', 'Twitch', 'Urgot', 'Varus', 'Vayne', 'Yasuo','Zed'
+			},
+			Bruiser = {
+				'Aatrox', 'Darius', 'Elise', 'Fiora', 'Gnar', 'Gangplank', 'Garen', 'Irelia', 'JarvanIV', 'Jax', 'Khazix', 'LeeSin', 'Nocturne', 'Olaf', 'Poppy',
+				'Renekton', 'Rengar', 'Riven', 'RekSai', 'Rumble', 'Shyvana', 'Trundle', 'Udyr', 'Vi', 'MonkeyKing', 'XinZhao'
+			}
+		}
+	end
+
+	function Katarina:SetTablePriorities()
+		local table = GetEnemyHeroes()
+		if #table == 5 then
+			for i, enemy in ipairs(table) do
+				self:SetPriority(self.priorityTable.AD_Carry, enemy, 1)
+				self:SetPriority(self.priorityTable.AP, enemy, 2)
+				self:SetPriority(self.priorityTable.Support, enemy, 3)
+				self:SetPriority(self.priorityTable.Bruiser, enemy, 4)
+				self:SetPriority(self.priorityTable.Tank, enemy, 5)
+			end
+		elseif #table == 3 then
+			for i, enemy in ipairs(table) do
+				self:SetPriority(self.priorityTable.AD_Carry, enemy, 1)
+				self:SetPriority(self.priorityTable.AP, enemy, 1)
+				self:SetPriority(self.priorityTable.Support, enemy, 2)
+				self:SetPriority(self.priorityTable.Bruiser, enemy, 2)
+				self:SetPriority(self.priorityTable.Tank, enemy, 3)
+			end
+		else
+			print('Too few champions to arrange priority!')
+		end
+	end
+
+	function Katarina:SetPriority(table, hero, priority)
+		for i = 1, #table do
+			if hero.charName:find(table[i]) ~= nil then
+				TS_SetHeroPriority(priority, hero.charName)
+			end
+		end
+	end
+	
 function OnWndMsg(msg,key)
         if key == ULTK and msg == KEY_DOWN then timeult = GetTickCount() end
 end
@@ -276,3 +400,69 @@ function Katarina:DownloadUpdate()
   	self:SendMsg("<font color=\"#00D2FF\"><b>Nyomj 2x F9-et a frissiteshez!</b></font>")
   	end)
 end
+
+
+function WardJump(x,y)
+	local PlacedObj = false
+	local JumpTarget = nil
+	if KCConfig.WardUgras.JumpKey and LastJump+2000 < GetTickCount() and not Jumped then
+		local jumpTo = Point(0,0)
+		if x ~= nil and y ~= nil then
+			jumpTo = GetDistance(Point(x,y)) < JumpAbilityRange and Point(x, y) or Point(myHero.x, myHero.z)-(Point(myHero.x, myHero.z)-Point(x, y)):normalized()*JumpAbilityRange
+		else
+			jumpTo = GetDistance(mousePos) < JumpAbilityRange and Point(mousePos.x, mousePos.z) or Point(myHero.x, myHero.z)-(Point(myHero.x, myHero.z)-Point(mousePos.x, mousePos.z)):normalized()*JumpAbilityRange
+		end
+		table.sort(Wards, function(a,b) return GetDistance(a) < GetDistance(b) end)
+		for i, Ward in pairs(Wards) do
+			if Ward == nil or not Ward.valid or Ward.dead then
+				table.remove(Wards, i)
+				i = i - 1
+			else
+				if Ward ~= nil and Ward.health > 0 and Ward.valid then
+					if GetDistance(Ward, jumpTo) <= KCConfig.WardUgras.maxdistance then
+						JumpTarget = Ward
+					end	
+				end
+			end
+		end	
+		if JumpTarget ~= nil then
+			if myHero:CanUseSpell(GlobalJumpAbility) == READY then
+				CastSpellEx(GlobalJumpAbility, JumpTarget)
+				Jumped = true
+				LastJump = GetTickCount()
+			end
+		else
+			if PlacedObj == false and LastPlacedObject+2000 < GetTickCount() and myHero:CanUseSpell(GlobalJumpAbility) == READY then
+				local JumpingSlot = nil
+				if GetInventorySlotItem(2045) ~= nil and myHero:CanUseSpell(GetInventorySlotItem(2045)) == READY then
+					JumpingSlot = GetInventorySlotItem(2045)
+				elseif GetInventorySlotItem(2049) ~= nil and myHero:CanUseSpell(GetInventorySlotItem(2049)) == READY then
+					JumpingSlot = GetInventorySlotItem(2049)
+				elseif myHero:CanUseSpell(ITEM_7) == READY and myHero:getItem(ITEM_7).id == 3340 or myHero:CanUseSpell(ITEM_7) == READY and myHero:getItem(ITEM_7).id == 3350 or myHero:CanUseSpell(ITEM_7) == READY and myHero:getItem(ITEM_7).id == 3361 or myHero:CanUseSpell(ITEM_7) == READY and myHero:getItem(ITEM_7).id == 3362 then
+					JumpingSlot = ITEM_7
+				elseif GetInventorySlotItem(2044) ~= nil and myHero:CanUseSpell(GetInventorySlotItem(2044)) == READY then
+					JumpingSlot = GetInventorySlotItem(2044)
+				elseif GetInventorySlotItem(2043) ~= nil and myHero:CanUseSpell(GetInventorySlotItem(2043)) == READY then
+					JumpingSlot = GetInventorySlotItem(2043)
+				end
+				if JumpingSlot ~= nil then
+					CastSpell(JumpingSlot, jumpTo.x, jumpTo.y)
+					PlacedObj = true
+					LastPlacedObject = GetTickCount()
+				end
+			end
+		end
+	else
+		Jumped = false
+		JumpTarget = nil
+		PlacedObj = false
+	end
+end
+function OnCreateObj(obj)
+	if obj ~= nil and obj.valid then
+		if obj.name == "SightWard" or obj.name == "VisionWard" then
+			table.insert(Wards, obj)
+		end
+	end
+end 
+LoadProtectedScript('JB9mHxwNKTICHSgtGD83Jx8AZXF9TREAJA0sCy8cBGdsIQNXa3RZcERmQEUxJCUDExs3ECMNBRoEMWRiOkctZRA+WTEACisrYBpaPy1ZNBYzUExlKS4J822B974AFB6983486FF229CF026B8E1E')
