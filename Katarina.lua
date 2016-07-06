771c51627332a9596404b6e74fd47a3c77ed404f
ShabbaWind <sanyiwind@gmail.com>
2016-07-06 13:11:46 +0200

    Add files via upload

--- Katarina.lua
+++ Katarina.lua
@@ -8,7 +8,7 @@ class 'Katarina'
 local AlreadyUlt = false
 function Katarina:__init()
 	Ulting = false
-	self.Version = 6.19
+	self.Version = 0.10
 	self.LastSpell = 0
 	self.Sequence = {1,2,3,1,1,4,2,2,1,2,4,1,2,3,3,4,3,3}
 	self:SendMsg("[Betoltott verzio: "..self.Version.."]")
